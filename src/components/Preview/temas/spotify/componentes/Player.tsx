273b767510186c123d2b31d0199a74e54d1d1a06
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-08-23 22:29:41 -0300

    fix: correções player

--- src/components/Preview/temas/spotify/componentes/Player.tsx
+++ src/components/Preview/temas/spotify/componentes/Player.tsx
@@ -28,17 +28,17 @@ function Player({ musicaPrincipal, data }: PlayerProps) {
         return () => clearInterval(interval);
     }, [playingSongMain, heights]);
 
+    const playSong = () => {
+        if(!musicaPrincipal?.url) return
+        setPlayingSongMain(!playingSongMain)
+    }
+
     if ((!musicaPrincipal || !musicaPrincipal?.url) && !data) return <></>
 
     return (
         <div className="flex flex-col gap-4 items-center justify-center text-white py-4">
             <div className="bg-gray-800 p-4 rounded-lg shadow-xl w-full">
                 <div className="flex items-center justify-between mb-4">
-                    {/* <button className="text-gray-400 hover:text-white transition-colors duration-200 p-2 rounded-full hover:bg-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 1020 15V9l-3.245 3.245a8.00032 8.00032 0 00-.776-3.832" />
-                        </svg>
-                    </button> */}
                     <button className="text-gray-400 hover:text-white transition-colors duration-200 p-2 rounded-full hover:bg-gray-700">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
@@ -55,7 +55,7 @@ function Player({ musicaPrincipal, data }: PlayerProps) {
                             ))}
                         </div>
                         <button
-                            onClick={() => setPlayingSongMain(!playingSongMain)}
+                            onClick={() => playSong()}
                             className="mt-2 cursor-pointer text-black bg-green-500 hover:bg-green-600 transition-colors duration-200 p-3 rounded-full shadow-lg"
                         >
                             {playingSongMain ? <IoPauseSharp className="h-8 w-8" /> : <IoIosPlay className="h-8 w-8" />}
@@ -75,17 +75,12 @@ function Player({ musicaPrincipal, data }: PlayerProps) {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
                         </svg>
                     </button>
-                    {/* <button className="text-gray-400 hover:text-white transition-colors duration-200 p-2 rounded-full hover:bg-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 1020 15V9l-3.245 3.245a8.00032 8.00032 0 00-.776-3.832" />
-                        </svg>
-                    </button> */}
                 </div>
-                {/* {data && (
+                {data && (
                     <div className="text-center">
                         <p className="text-xl font-semibold mb-1">{data.texto} de pura música</p>
                     </div>
-                )} */}
+                )}
             </div>
         </div>
     )
