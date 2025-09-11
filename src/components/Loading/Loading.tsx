8107310a6fda1ae6cf8f9060c4537affb7f868e0
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-09-10 23:37:41 -0300

    refactor: altera estilo para deixa texto no centro

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = ({ text, size }: { text?: string, size: number }) => {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white/20 backdrop-blur-md z-50">
       <BeatLoader color="#DA70D6" size={size} />
-      {text && <p className="mt-12 text-[#DA70D6] text-lg font-medium">{text}</p>}
+      {text && <p className="text-center mt-12 text-[#DA70D6] text-lg font-medium">{text}</p>}
     </div>
   );
 };
