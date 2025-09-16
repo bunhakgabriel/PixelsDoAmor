f7b4aa004b4a34443c23846a683c0e1b122ca82a
gabriel bunhak <bunhakgabriel@gmail.com>
2025-09-16 16:08:23 -0300

    feat: implementa loadblock que nao mostra o fundo da tela

---
+++ src/components/Loading/LoadingBlock.tsx
@@ -0,0 +1,12 @@
+import { BeatLoader } from 'react-spinners';
+
+const LoadingBlock = ({ text, size }: { text?: string, size: number }) => {
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-white z-50">
+      <BeatLoader color="#DA70D6" size={size} />
+      {text && <p className="text-center mt-12 text-[#DA70D6] text-lg font-medium">{text}</p>}
+    </div>
+  );
+};
+
+export default LoadingBlock;
--- src/components/ScrollToTop.tsx/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx/ScrollToTop.tsx
@@ -1,13 +1,23 @@
 // ScrollToTop.tsx
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import LoadingBlock from "../Loading/LoadingBlock";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         window.scrollTo({ top: 0, behavior: "smooth" });
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
     }, [pathname]);
 
-    return null;
+    if(loading){
+        return <LoadingBlock text="Carregando..." size={30} />
+    } else {
+        return null;
+    }
 }
