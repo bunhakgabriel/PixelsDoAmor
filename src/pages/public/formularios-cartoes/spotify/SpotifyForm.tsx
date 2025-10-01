4c7892cd2bbb79e65cc278c1b944905dcb1f0b29
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-10-01 13:44:58 -0300

    Revert "feat: adiciona alerta de progresso ao abrir a modal de pré-visualização do cartão"
    This reverts commit 006cdc3f6c1b3e87ff4b80868e19a2b288395a2f.

--- src/pages/public/formularios-cartoes/spotify/SpotifyForm.tsx
+++ src/pages/public/formularios-cartoes/spotify/SpotifyForm.tsx
@@ -55,7 +55,6 @@ function SpotifyForm() {
 
   const { previewCartao, setPreviewCartao, data, setData } = useConfigStoreSpotify();
   const [etapaAtual, setEtapaAtual] = useState(0);
-  const [firstOpenModal, setFirstOpenModal] = useState(true);
   const model = useWatch<ISpotifyModel>({ control }) as ISpotifyModel;
 
   async function avancarEtapa() {
@@ -92,22 +91,12 @@ function SpotifyForm() {
     setEtapaAtual((prev) => prev - 1);
   }
 
-  function scrollToTop() {
+  function scrollToTop(){
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   };
-
-  function previaCartao() {
-    setPreviewCartao(true)
-    setTimeout(() => {
-      if (firstOpenModal) {
-        alert('Para não perder seu progresso, clique no "X" no canto superior direito para fechar a modal e continuar!')
-        setFirstOpenModal(false)
-      }
-    }, 1000)
-  }
 
   useEffect(() => {
     console.log("Model atualizado:", data);
@@ -158,7 +147,7 @@ function SpotifyForm() {
             element="button"
           />
           <button
-            onClick={() => previaCartao()}
+            onClick={() => setPreviewCartao(true)}
             className="cursor-pointer inline-flex items-center justify-center space-x-2 bg-white text-purple-600 px-6 py-2 sm:px-8 sm:py-4 rounded-xl text-base sm:text-lg font-semibold hover:bg-gray-100 transition-all duration-300 shadow-lg hover:shadow-xl transform hover:-translate-y-1"
           >
             <HiOutlineSparkles className="w-5 h-5" />
